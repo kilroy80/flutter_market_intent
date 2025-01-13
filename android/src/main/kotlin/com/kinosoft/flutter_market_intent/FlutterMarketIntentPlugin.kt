4b2149799c29ae3cdd49151a0016f29df138f30d
kilroy80 <kilroy80@gmail.com>
2025-01-13 21:17:41 +0900

    feat: Update code

--- android/src/main/kotlin/com/kinosoft/flutter_market_intent/FlutterMarketIntentPlugin.kt
+++ android/src/main/kotlin/com/kinosoft/flutter_market_intent/FlutterMarketIntentPlugin.kt
@@ -71,14 +71,16 @@ class FlutterMarketIntentPlugin: FlutterPlugin, MethodCallHandler {
       val intent: String = call.argument("intent") ?: ""
       val i = Intent.parseUri(intent, Intent.URI_INTENT_SCHEME)
 
-//      if (i.resolveActivity(context.packageManager) != null) {
-//        val existPackage = context.packageManager.getLaunchIntentForPackage(i.`package`)
-//        context.startActivity(i)
-//        result.success(null)
-//      } else {
+      try {
+        val existPackage = context.packageManager.getLaunchIntentForPackage(i.`package` ?: "")
+        if (existPackage != null) {
+          context.startActivity(i)
+          result.success(null)
+        }
+      } catch (e: Exception) {
         val fallbackUrl = i.getStringExtra("browser_fallback_url") ?: ""
         result.success(fallbackUrl)
-//      }
+      }
 
     } else {
       result.notImplemented()
